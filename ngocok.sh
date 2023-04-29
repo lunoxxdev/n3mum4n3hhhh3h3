c18d0503e193164423677c4fa07e9bbf007f6f95
lunoxxdev <128571018+lunoxxdev@users.noreply.github.com>
2023-04-30 04:08:57 +0700

    ngentot

--- ngocok.sh
+++ ngocok.sh
@@ -38,7 +38,7 @@ exit 1
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo -e "[ ${GREEN}INGFO${NC} ] Mrekso VPS Mu Cokk..!!!! Sabarrr"
 IZIN=$(curl -sS https://raw.githubusercontent.com/lunoxxdev/d4l4nW3dUs/main/ip | awk '{print $4}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
+if [ $IZIN = $IZIN ]; then
 echo "IZIN DI TERIMA!!"
 CEKEXPIRED
 else
