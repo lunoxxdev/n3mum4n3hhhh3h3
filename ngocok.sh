c0a20d38467f3d94dac74b9131fc016d0189e787
lunoxxdev <128571018+lunoxxdev@users.noreply.github.com>
2023-04-30 03:46:47 +0700

    ngentot

--- ngocok.sh
+++ ngocok.sh
@@ -37,6 +37,7 @@ exit 1
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo -e "[ ${GREEN}INGFO${NC} ] Mrekso VPS Mu Cokk..!!!! Sabarrr"
 sleep 0.5
+}
 IZIN=$(curl -sS https://raw.githubusercontent.com/lunoxxdev/d4l4nW3dUs/main/ip | awk '{print $4}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 echo -e "[ ${GREEN}INGFO${NC} ] Monggo Masse :)"
