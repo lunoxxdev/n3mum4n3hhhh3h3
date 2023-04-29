dc21d8e3f19c88bed34e8cf7ec97c4a85c7917f2
lunoxxdev <128571018+lunoxxdev@users.noreply.github.com>
2023-04-30 04:05:43 +0700

    ngentot

--- ngocok.sh
+++ ngocok.sh
@@ -37,7 +37,6 @@ exit 1
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo -e "[ ${GREEN}INGFO${NC} ] Mrekso VPS Mu Cokk..!!!! Sabarrr"
-fi
 IZIN=$(curl -sS https://raw.githubusercontent.com/lunoxxdev/d4l4nW3dUs/main/ip | awk '{print $4}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 echo "IZIN DI TERIMA!!"
