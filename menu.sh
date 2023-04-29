3758fd2e3c1eb205315aaf9265ab03b3e68f633f
lunoxxdev <128571018+lunoxxdev@users.noreply.github.com>
2023-04-30 04:25:21 +0700

    ngentot

--- menu.sh
+++ menu.sh
@@ -57,6 +57,7 @@ wget -q -O /root/status "https://raw.githubusercontent.com/lunoxxdev/n3mum4n3hhh
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo -e "[ ${GREEN}INGFO${NC} ] Mrekso VPS Mu Cokk..!!!! Sabarrr"
+sleep 2
 IZIN=$(curl -sS https://raw.githubusercontent.com/lunoxxdev/d4l4nW3dUs/main/ip | awk '{print $4}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 echo "IZIN DI TERIMA!!"
